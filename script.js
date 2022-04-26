7d0ee6b350410c7c175935e3c5a47b0fdf59f17f
ebukanas <43076966+ebukanas@users.noreply.github.com>
2022-04-26 17:49:26 +0300

    Update script.js
    updated

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+//variable declarations
+const submitBtn = document.getElementById('submit');
+const rating = document.getElementsByClassName('rating');
+var userRating;
+var ratingSubmitted = false;
 
-const pointsTotal = 5;
+//run this function when you click on one of the rating buttons
+for (let i=0;i<rating.length;i++) {
+rating[i].addEventListener("click", function() {
+userRating = i+1;
+ratingSubmitted = true;
 
-const rating = [document.getElementsByClassName("rating")];
-const rating = [document.getElementById("star1")];
+//remove the styling from previous buttons
+for (let j=0; j<rating.length; j++) {
+	rating[j].style.backgroundColor = 'hsl(213, 19%, 21%)';
+  rating[j].style.color = 'hsl(216, 12%, 54%)';
+}
+//change the rating button styling on click
+rating[i].style.backgroundColor = 'hsl(25, 97%, 53%)';
+rating[i].style.color = 'white';
+});
+}
+
+//clicking on the submit button
+submitBtn.addEventListener("click", function() {
+//check if the user has submitted a rating
+	if (ratingSubmitted === true) {
+console.log('user rating is ' + userRating);
+//open the thankyou.html window
+window.open("thankyou.html","_self")
+//if the user hasn't selected a rating - alert them to do it.
+} else if (ratingSubmitted === false) {
+alert('Please select a rating');
+}
+
+})
+
+
+
+
 

50a767af350b6ea5434c388fd88b5dc6e891a062
ebukanas <edvinas.bukanas@gmail.com>
2022-05-03 10:18:13 +0300

    updated the js to work

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const rating = document.getElementsByClassName('rating');
 var userRating;
 var ratingSubmitted = false;
 
+
 //run this function when you click on one of the rating buttons
 for (let i=0;i<rating.length;i++) {
 rating[i].addEventListener("click", function() {
@@ -21,19 +22,43 @@ rating[i].style.color = 'white';
 });
 }
 
+//submitBtn is null because it is not declared in thankyou.html and 
+//im not sure how else I can get around this.
+if (submitBtn != null) {
+
 //clicking on the submit button
 submitBtn.addEventListener("click", function() {
 //check if the user has submitted a rating
 	if (ratingSubmitted === true) {
 console.log('user rating is ' + userRating);
+//store the rating in localStorage
+localStorage.setItem('user rating', userRating)
 //open the thankyou.html window
 window.open("thankyou.html","_self")
+
+// testing (can be removedxxw)
+// var ThankYouPage = window.open("thankyou.html","_self")
+// ThankYouPage.document.userRating.value = 'value from page1';
+
+
 //if the user hasn't selected a rating - alert them to do it.
 } else if (ratingSubmitted === false) {
 alert('Please select a rating');
 }
 
 })
+//if submitBrn is null - do this
+} else {
+
+	var alertRating = document.querySelector('.alertRating');
+	//get the previously stored rating value from local storage
+	let userRatingString = localStorage.getItem('user rating')
+
+	alertRating.textContent = 'You selected ' + userRatingString + ' out of 5';
+
+}
+
+
 
 
 
